1ebcff0914834f4425fe9cd7d5846b3b07b00caa
indy032229@gmail.com <indy032229@gmail.com>
2025-10-23 11:54:13 +0900

    2025/10/23

---
+++ 07_slot/src/App.vue
@@ -0,0 +1,24 @@
+<!-- 다름 파일을 불러올 때는 상대경로(./) 또는 src부터의 경로인 절대경로(@)를 사용할 수 있다. -->
+<script setup>
+  // import MyComponent from './components/01_component/MyComponent.vue';
+  import MyComponent from '@/components/01_component/MyComponent.vue';
+  import SlotParent from './components/02_slotintro/SlotParent.vue';
+
+</script>
+
+<template>
+  <header>
+    <div class="redColor">
+      <MyComponent/>
+    </div>
+  </header>
+  <main>
+    <SlotParent/>
+  </main>
+</template>
+
+<style scoped>
+  .redColor {
+    color: red;
+  }
+</style>
---
+++ 07_slot/src/components/03_namedslot/NamedParent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <h1>NamedSlot</h1>
+        <NamedChild>
+            <template v-slot:name>
+                <h2>
+                    name: {{ obj.name }}
+                </h2>
+            </template>
+            <template v-slot:founder>
+                <h2>
+                    name: {{ obj.founder }}
+                </h2>
+            </template>
+            <template v-slot:img>
+                <h2>
+                    name: {{ obj.img }}
+                </h2>
+            </template>
+        </NamedChild>
+    </div>
+</template>
+
+<script setup>
+    import NamedChild from './NamedChild.vue';
+    import {reactive} from 'vue';
+
+    const obj = reactive({
+        name: "vue",
+        founder: "Evan You",
+        img: "https://images.ctfassets.net/s5uo95nf6njh/2fEr6ctL9FxPtOPhUcM4FA/773007e573bfa16aebdb736f767b45fa/evan-you-hero.jpg"
+    });
+</script>
+
+<style scoped>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border: 1px solid black;
+    }
+    img {
+        width: 200px;
+        height: 200px;
+    }
+</style>
