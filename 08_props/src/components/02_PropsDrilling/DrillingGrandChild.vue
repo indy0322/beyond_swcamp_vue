1dfe06d5f20dd08fe84c23597ad2c602fca8672e
indy032229@gmail.com <indy032229@gmail.com>
2025-10-23 15:03:50 +0900

    2025/10/23

---
+++ 08_props/src/components/02_PropsDrilling/DrillingGrandChild.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>GrandChild: {{ injectedCount }}</h3>
+    </div>
+</template>
+
+<script setup>
+    import {inject} from 'vue';
+
+    const injectedCount = inject('count');
+</script>
+
+<style scoped>
+
+</style>
---
+++ 08_props/src/components/02_PropsDrilling/DrillingParent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <DrillingChild />
+    </div>
+</template>
+
+<script setup>
+    import DrillingChild from './DrillingChild.vue';
+    import {ref, provide} from 'vue';
+    const count = ref(2);
+    provide('count', count);
+</script>
+
+<style scoped>
+
+</style>
