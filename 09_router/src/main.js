f58c1a5db83498c6703aa459aa094ad53ea8bfa6
indy032229@gmail.com <indy032229@gmail.com>
2025-10-24 10:53:52 +0900

    2025/10/24

--- 09_router/src/main.js
+++ 09_router/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/01_router.js';
+// import router from './router/01_router.js';
+import router2 from './router/02_nestedRouter.js';
 
 /* npm install vue-router@next로 dependency 추가 */
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app');
+const app = createApp(App);
+// app.use(router);
+app.use(router2);
+app.mount('#app');
